4d3eda9dccc19975e16c9d43473e9d09f9115c77
unknown <hazardland@gmail.com>
2019-07-31 12:26:28 +0400

    added config.js for train.html and draw.html

---
+++ config.js
@@ -0,0 +1,26 @@
+var config = {
+    name: 'fast1',
+    width: 10,
+    height: 10,
+    scale: 20,
+    speed: 0,
+    demo:500,
+    train:200000,
+    creature: {
+        count: 1000,
+        energy: 400,
+        laziness: 4
+
+    },
+    food: {
+        count:0,
+        energy: 100
+    },
+    wall: {
+        count:3
+    },
+    brain:{
+        structure: [8,4],
+        change: 0.1
+    }
+};
